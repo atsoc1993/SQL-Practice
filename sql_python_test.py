6d79b7784fb86efe83a735e8875a7fe2c8dcd4f4
atsoc1993 <atsoc1993@gmail.com>
2025-10-28 16:36:18 -0400

    Added company table, additional keys to order table, alter order table method, singular/multiple company insert methods and getter, added sides to orders eg; bid or ask, and hashmaps for easy referencing, created orders for multiple users

--- sql_python_test.py
+++ sql_python_test.py
@@ -56,23 +56,71 @@ def create_user_table() -> None:
     print(f'Created User Table')
     return
 
+def create_company_table() -> None:
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS `Company` (
+                StockID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                CompanyName VARCHAR(50) NOT NULL,
+                AssetTicker VARCHAR(10) NOT NULL
+            );
+            """
+        )
+        print(f'Created Company Table')
+
 def create_order_table() -> None:
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS `Order` (
             OrderID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
             UserID INT NOT NULL,
-            CONSTRAINT fk_orders_user_id
+            StockID INT NOT NULL,
+            CONSTRAINT fk_order_user_ids
                 FOREIGN KEY (UserID)
                     REFERENCES `User`(UserID)
+                    ON DELETE RESTRICT
+                    ON UPDATE CASCADE,
+            CONSTRAINT fk_order_stock_ids
+                FOREIGN KEY (StockID)
+                    REFERENCES `Company`(StockID)
                     ON DELETE RESTRICT
                     ON UPDATE CASCADE
         );
         """
     )
-    print(f'Created User Table')
+    print(f'Created Order Table')
     return
 
+
+
+def alter_order_table_to_include_bids_and_ask_type() -> None:
+    cur.execute(
+        """
+        ALTER TABLE `Order`
+            ADD COLUMN `Side` TINYINT(1) NOT NULL DEFAULT 1,
+            ADD CONSTRAINT valid_bid_type CHECK (`Side` IN (0, 1));
+        """
+    )
+    print(f'Added Bids & Asks to Order Table')
+
+
+
+'''
+General Table Creation & Alteration
+'''
+drop_db_if_exists()
+create_db_if_not_exists()
+use_db()
+create_user_table()
+create_company_table()
+create_order_table()
+
+alter_order_table_to_include_bids_and_ask_type()
+
+
+'''
+User Table Mods & Getters Test
+'''
 def create_singular_user(full_name: str) -> None:
     cur.execute("INSERT INTO `User` (UserFullName) VALUES (%s)", (full_name,))
     print(f'Inserted user\'s full name into User Table')
@@ -95,16 +143,6 @@ def get_all_user_full_names() -> list[str]:
     users = get_all_users()
     return [user['UserFullName'] for user in users]
 
-drop_db_if_exists()
-create_db_if_not_exists()
-use_db()
-create_user_table()
-create_order_table()
-
-
-'''
-User Table Mods & Getters Test
-'''
 test_user_1_name = "Leo Costa"
 create_singular_user(full_name=test_user_1_name)
 
@@ -127,12 +165,53 @@ print(f'All Current User Full Names: {all_user_full_names}')
 
 
 '''
+Company Table Mods & Getters Test
+'''
+def create_singular_company(company_name: str, asset_ticker: str) -> None:
+    cur.execute("INSERT INTO `Company` (CompanyName,AssetTicker) VALUES (%s, %s)", (company_name, asset_ticker))
+    print(f'Inserted company\'s name & ticker into Company Table')
+    return 
+
+def create_multiple_companies(companies_info: list[tuple[str,str]]) -> None:
+    cur.executemany("INSERT INTO `Company` (CompanyName,AssetTicker) VALUES (%s, %s)", (companies_info))
+    print(f'Inserted MULTIPLE companies\' names & tickes into Company Table')
+    return 
+
+def get_all_companies() -> list[dict]:
+    cur.execute("SELECT * FROM `Company`")
+    return cur.fetchall()
+
+test_company_1_details = ("Test Company #1", 'TC1')
+create_singular_company(test_company_1_details[0], test_company_1_details[1])
+
+test_company_2_details = ("Test Company #2", 'TC2')
+test_company_3_details = ("Test Company #3", 'TC3')
+test_company_4_details = ("Test Company #4", 'TC4')
+create_multiple_companies(
+    (test_company_2_details, test_company_3_details, test_company_4_details)
+)
+
+all_companies = get_all_companies()
+print(f'All Companies: {all_companies}')
+
+'''
 Order Table Mods & Getters Test
 '''
 
-def create_order_for_user(user_id: int) -> None:
-    cur.execute("INSERT INTO `Order` (UserID) VALUES (%s)", (1))
-    print(f'Created Order for User ID: {user_id}')
+mini_side_hashmap = {
+    'Bid': 0,
+    'Ask': 1,
+}
+
+mini_reverse_side_hashmap = {
+    0: 'Bid',
+    1: 'Ask'
+}
+
+
+def create_order_for_user(user_id: int, stock_id: int, bid_or_ask: str) -> None:
+    cur.execute("INSERT INTO `Order` (UserID, StockID, `Side`) VALUES (%s, %s, %s)", (user_id, stock_id, mini_side_hashmap[bid_or_ask]))
+    print(f'Created {mini_reverse_side_hashmap[mini_side_hashmap[bid_or_ask]]} Order of Stock ID: {stock_id} for User ID: {user_id}')
     return
 
 def get_all_orders() -> list[dict]:
@@ -143,6 +222,10 @@ def get_all_order_ids() -> list[dict]:
     orders = get_all_orders()
     return [order['OrderID'] for order in orders]
 
-create_order_for_user(1)
-all_current_order_ids = get_all_order_ids()
-print(f'All Current Order IDs: {all_current_order_ids}')
+create_order_for_user(1, 1, 'Bid')
+create_order_for_user(2, 1, 'Ask')
+create_order_for_user(3, 2, 'Bid')
+all_current_orders = get_all_orders()
+print(f'All Current Orders: {all_current_orders}')
+# all_current_order_ids = get_all_order_ids()
+# print(f'All Current Order IDs: {all_current_order_ids}')
