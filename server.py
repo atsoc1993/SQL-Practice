5294f2070eafbe45bc247e55cbd3492eb9e394ff
atsoc1993 <atsoc1993@gmail.com>
2025-10-30 15:25:55 -0400

    Added base models and endpoints for users & companies

--- server.py
+++ server.py
@@ -3,15 +3,83 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Integer, String, select
+from sqlalchemy.orm import Session, MappedAsDataclass, Mapped, mapped_column, DeclarativeBase
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     middleware_class=CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
+    allow_methods=["*"]
 )
 
-@app.get('/')
-def test():
-    print("Success")
-    return {"message": "Success"}
+user = "root"
+password = os.getenv('PASSWORD')
+host = os.getenv('HOST')
+db = "LEOCOSTA_SQLALCHEMY_ORDERBOOK_TEST"
+base_url = f"mysql+pymysql://{user}:{password}@{host}"
+
+class Base(DeclarativeBase):
+    pass
+
+class User(MappedAsDataclass, Base):
+    __tablename__ = "User"
+    UserID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
+    UserFullName: Mapped[str] = mapped_column(String(50), nullable=False)
+
+
+class UserInfo(BaseModel):
+    full_name: str
+
+class Company(MappedAsDataclass, Base):
+    __tablename__ = "Company"
+    StockID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
+    CompanyName: Mapped[str] = mapped_column(String(50), nullable=False)
+    AssetTicker: Mapped[str] = mapped_column(String(10), nullable=False)
+
+class CompanyInfo(BaseModel):
+    name: str
+    ticker: str
+
+
+    
+@app.get('/get_users')
+def get_users():
+    engine = create_engine(f'{base_url}/{db}')
+    with Session(engine) as session:
+        users = session.scalars(select(User)).all()
+        return [{'user_id': user.UserID, 'full_name': user.UserFullName} for user in users]
+
+@app.post('/add_user')
+def add_user(payload: UserInfo) -> int:
+    engine = create_engine(f'{base_url}/{db}')
+    print(f'Received User Info: {payload}')
+    with Session(engine) as session:
+        user = User(UserFullName=payload.full_name)
+        session.add(user)
+        session.commit()
+        return user.UserID
+
+
+@app.get('/get_companies')
+def get_users() -> list[dict, any]:
+    engine = create_engine(f'{base_url}/{db}')
+    with Session(engine) as session:
+        companies = session.scalars(select(Company)).all()
+        return [{'stock_id': company.StockID, 'company_name': company.CompanyName, 'asset_ticker': company.AssetTicker} for company in companies]
+
+@app.post('/add_company')
+def add_user(payload: CompanyInfo) -> int:
+    engine = create_engine(f'{base_url}/{db}')
+    print(f'Received Company Info: {payload}')
+    with Session(engine) as session:
+        company = Company(CompanyName=payload.name, AssetTicker=payload.ticker)
+        session.add(company)
+        session.commit()
+        return company.StockID
