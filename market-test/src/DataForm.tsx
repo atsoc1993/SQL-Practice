f15ac23bab63c5ee9646b08ed46f08e8b7fabede
atsoc1993 <atsoc1993@gmail.com>
2025-10-30 15:26:31 -0400

    Styled data form and added getters for users & companies

--- market-test/src/DataForm.tsx
+++ market-test/src/DataForm.tsx
@@ -1,30 +1,55 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import axios from 'axios'
 
 export default function DataForm() {
 
+    type UserInfo = {
+        full_name: string | undefined
+    }
+
+    type CompanyInfo = {
+        name: string | undefined,
+        ticker: string | undefined
+    }
+
+    type OrderInfo = {
+        user_id: number | undefined,
+        stock_id: number | undefined,
+        is_bid: boolean | undefined
+    }
+    const userInfoTemplate: UserInfo = {
+        full_name: undefined
+    }
+
+    const companyInfoTemplate: CompanyInfo = {
+        name: undefined,
+        ticker: undefined,
+    }
+
+    const orderInfoTemplate: OrderInfo = {
+        user_id: undefined,
+        stock_id: undefined,
+        is_bid: undefined
+    }
+
     const [bidding, setBidding] = useState(true);
+    const [userInfo, setUserInfo] = useState<UserInfo>(userInfoTemplate);
+    const [companyInfo, setCompanyInfo] = useState<CompanyInfo>(companyInfoTemplate);
+    const [orderInfo, setOrderInfo] = useState<OrderInfo>(orderInfoTemplate);
+
+
+    const insertUser = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/add_user', userInfo)
+        console.log(response.data)
+    }
+    const insertCompany = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/add_company', companyInfo)
+        console.log(response.data)
+    }
+
 
     return (
         <div className='h-full bg-slate-300 flex min-w-full mx-auto flex-col items-center'>
-            <div className='h-1/3 bg-green-300 flex min-w-full mx-auto flex-col items-center'>
-
-                <div className='h-full flex min-w-full mx-auto justify-center flex-row items-center'>
-
-                    <div className='h-full flex w-1/4 min-w-fit justify-center flex-col items-center'>
-                        <input className='min-w-fit shadow-md shadow-slate-600 m-5 h-fit p-6 rounded-xl text-center focus:outline-none'
-                            placeholder='Enter Company Name'
-                        ></input>
-                        <input className='min-w-fit shadow-md shadow-slate-600 m-5 h-fit p-6 rounded-xl text-center focus:outline-none'
-                            placeholder='Enter Abbreviation'
-                        ></input>
-                    </div>
-
-                    <button className='min-w-fit bg-emerald-200 w-1/4 shadow-md shadow-slate-600 h-fit p-6 m-10 rounded-xl text-center'>
-                        Insert Company </button>
-
-                </div>
-
-            </div>
             <div className='h-1/3 bg-blue-400 flex min-w-full mx-auto flex-col items-center'>
 
                 <div className='h-full flex min-w-full mx-auto justify-center flex-row items-center'>
@@ -32,11 +57,40 @@ export default function DataForm() {
                     <div className='h-full flex w-1/4 min-w-fit justify-center flex-col items-center'>
                         <input className='min-w-fit shadow-md shadow-slate-600 m-5 h-fit p-6 rounded-xl text-center focus:outline-none'
                             placeholder="Enter User's Full Name"
+                            onChange={(e) => setUserInfo(prev => ({ ...prev, full_name: e.target.value }))}
+
                         ></input>
                     </div>
 
-                    <button className='min-w-fit bg-blue-300 w-1/4 shadow-md shadow-slate-600 h-fit p-6 m-10 rounded-xl text-center'>
+                    <button className='min-w-fit bg-blue-300 w-1/4 shadow-md shadow-slate-600 h-fit p-6 m-10 rounded-xl text-center'
+                        onClick={async () => insertUser()}
+                    >
                         Insert User</button>
+
+                </div>
+
+            </div>
+            <div className='h-1/3 bg-green-300 flex min-w-full mx-auto flex-col items-center'>
+
+                <div className='h-full flex min-w-full mx-auto justify-center flex-row items-center'>
+
+                    <div className='h-full flex w-1/4 min-w-fit justify-center flex-col items-center'>
+                        <input className='min-w-fit shadow-md shadow-slate-600 m-5 h-fit p-6 rounded-xl text-center focus:outline-none'
+                            placeholder='Enter Company Name'
+                            onChange={(e) => setCompanyInfo(prev => ({ ...prev, name: e.target.value }))}
+
+                        ></input>
+                        <input className='min-w-fit shadow-md shadow-slate-600 m-5 h-fit p-6 rounded-xl text-center focus:outline-none'
+                            placeholder='Enter Abbreviation'
+                            onChange={(e) => setCompanyInfo(prev => ({ ...prev, ticker: e.target.value }))}
+                        ></input>
+                    </div>
+
+                    <button className='min-w-fit bg-emerald-200 w-1/4 shadow-md shadow-slate-600 h-fit p-6 m-10 rounded-xl text-center'
+                        onClick={async () => insertCompany()}
+
+                    >
+                        Insert Company </button>
 
                 </div>
 
