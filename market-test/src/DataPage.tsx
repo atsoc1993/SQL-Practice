329ae772700b5875a3319cad6e861a0ab2a40862
atsoc1993 <atsoc1993@gmail.com>
2025-10-30 22:12:06 -0400

    Added orders to UI

--- market-test/src/DataPage.tsx
+++ market-test/src/DataPage.tsx
@@ -4,34 +4,53 @@ import axios from 'axios';
 export default function DataPage() {
 
     type UserInfoResponse = {
-        user_id: number,
-        full_name: string
-    }
+        user_id: number;
+        full_name: string;
+    };
 
     type CompanyInfoResponse = {
-        stock_id: number,
-        company_name: string,
-        asset_ticker: string
-    }
+        stock_id: number;
+        company_name: string;
+        asset_ticker: string;
+    };
+
+    type OrderInfoResponse = {
+        user_id: number;
+        stock_id: string;
+        order_id: number;
+        is_bid: string;
+    };
 
 
     const [users, setUsers] = useState<UserInfoResponse[]>([]);
     const [companies, setCompanies] = useState<CompanyInfoResponse[]>([]);
+    const [orders, setOrders] = useState<OrderInfoResponse[]>([]);
+
+
 
     const getUsers = async () => {
         const response = await axios.get('http://127.0.0.1:8000/get_users')
         setUsers(response.data)
-    }
+    };
+
     const getCompanies = async () => {
         const response = await axios.get('http://127.0.0.1:8000/get_companies')
         console.log(response.data)
         setCompanies(response.data)
-    }
+    };
+
+    const getOrders = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/get_orders')
+        console.log(response.data)
+        setOrders(response.data)
+    };
 
     useEffect(() => {
         getUsers();
         getCompanies();
+        getOrders();
     }, [])
+
 
     return (
         <div className="min-w-full min-h-full bg-orange-200">
@@ -47,7 +66,7 @@ export default function DataPage() {
                     <table className="w-full border-t border-slate-200">
                         <thead className="bg-blue-300">
                             <tr className=" text-slate-600 divide-y border-b">
-                                <th className="px-5 py-3 text-sm font-medium">ID</th>
+                                <th className="px-5 py-3 text-sm font-medium">User ID</th>
                                 <th className="px-5 py-3 text-sm font-medium">Full name</th>
                             </tr>
                         </thead>
@@ -70,42 +89,82 @@ export default function DataPage() {
                         </tbody>
                     </table>
                 </div>
+            </div>
+
+            <div className="mx-auto w-5/12 max-w-5xl rounded-xl shadow-md shadow-slate-600 m-10">
+                <div className="flex items-center justify-between px-5 py-4 bg-green-300">
+                    <h2 className="text-2xl font-semibold">Companies</h2>
+                    <span className="text-sm text-slate-500">{companies.length} total</span>
                 </div>
-                <div className="mx-auto w-5/12 max-w-5xl rounded-xl shadow-md shadow-slate-600 m-10">
-                    <div className="flex items-center justify-between px-5 py-4 bg-green-300">
-                        <h2 className="text-2xl font-semibold">Companies</h2>
-                        <span className="text-sm text-slate-500">{companies.length} total</span>
-                    </div>
 
-                    <div className="overflow-x-auto">
-                        <table className="w-full border-t border-slate-200 ">
-                            <thead className='bg-emerald-200'>
-                                <tr className=" text-slate-600 divide-y border-b">
-                                    <th className="px-5 py-3 text-sm font-medium">Company Name</th>
-                                    <th className="px-5 py-3 text-sm font-medium">Ticker</th>
-                                    <th className="px-5 py-3 text-sm font-medium">Stock ID</th>
+                <div className="overflow-x-auto">
+                    <table className="w-full border-t border-slate-200 ">
+                        <thead className='bg-emerald-200'>
+                            <tr className=" text-slate-600 divide-y border-b">
+                                <th className="px-5 py-3 text-sm font-medium">Stock ID</th>
+                                <th className="px-5 py-3 text-sm font-medium">Company Name</th>
+                                <th className="px-5 py-3 text-sm font-medium">Ticker</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-slate-200 bg-emerald-200">
+                            {users.length === 0 ? (
+                                <tr>
+                                    <td colSpan={3} className="px-5 py-10 text-center text-slate-500">
+                                        No Companies yet.
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody className="divide-y divide-slate-200 bg-emerald-200">
-                                {users.length === 0 ? (
-                                    <tr>
-                                        <td colSpan={3} className="px-5 py-10 text-center text-slate-500">
-                                            No Companies yet.
-                                        </td>
-                                    </tr>
-                                ) : (
-                                    companies.map(c => (
-                                        <tr key={c.company_name} className="hover:bg-green-300 text-center">
-                                            <td className="px-5 py-3 text-sm text-slate-700">{c.company_name}</td>
-                                            <td className="px-5 py-3 text-sm text-slate-900 font-medium">{c.asset_ticker}</td>
-                                            <td className="px-5 py-3 text-sm text-slate-900 font-medium">{c.stock_id}</td>
+                            ) : (
+                                companies.map(c => (
+                                    <tr key={c.company_name} className="hover:bg-green-300 text-center">
+                                        <td className="px-5 py-3 text-sm text-slate-900 font-medium">{c.stock_id}</td>
+                                        <td className="px-5 py-3 text-sm text-slate-700">{c.company_name}</td>
+                                        <td className="px-5 py-3 text-sm text-slate-900 font-medium">{c.asset_ticker}</td>
 
-                                        </tr>
-                                    ))
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="mx-auto w-5/12 max-w-5xl rounded-xl shadow-md shadow-slate-600 m-10">
+                <div className="flex items-center justify-between px-5 py-4 bg-red-300">
+                    <h2 className="text-2xl font-semibold">Orders</h2>
+                    <span className="text-sm text-slate-500">{orders.length} total</span>
+                </div>
+
+                <div className="overflow-x-auto">
+                    <table className="w-full border-t border-slate-200 ">
+                        <thead className='bg-pink-300'>
+                            <tr className=" text-slate-600 divide-y border-b">
+                                <th className="px-5 py-3 text-sm font-medium">Order ID</th>
+                                <th className="px-5 py-3 text-sm font-medium">Stock ID</th>
+                                <th className="px-5 py-3 text-sm font-medium">User ID</th>
+                                <th className="px-5 py-3 text-sm font-medium">Bid</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-slate-200 bg-pink-300">
+                            {users.length === 0 ? (
+                                <tr>
+                                    <td colSpan={4} className="px-5 py-10 text-center text-slate-500">
+                                        No Companies yet.
+                                    </td>
+                                </tr>
+                            ) : (
+                                orders.map(o => (
+                                    <tr key={o.order_id} className="hover:bg-green-300 text-center">
+                                        <td className="px-5 py-3 text-sm text-slate-700">{o.order_id}</td>
+                                        <td className="px-5 py-3 text-sm text-slate-900 font-medium">{o.stock_id}</td>
+                                        <td className="px-5 py-3 text-sm text-slate-900 font-medium">{o.user_id}</td>
+                                        <td className="px-5 py-3 text-sm text-slate-900 font-medium">{o.is_bid ? 'Bid' : 'Ask'}</td>
+
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
