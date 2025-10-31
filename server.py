7d1cf37704424c7a14161455484175ea3b543f7b
atsoc1993 <atsoc1993@gmail.com>
2025-10-30 22:09:38 -0400

    Added order endpoints

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Integer, String, select
+from sqlalchemy import create_engine, Integer, String, Boolean, select
 from sqlalchemy.orm import Session, MappedAsDataclass, Mapped, mapped_column, DeclarativeBase
 import os
 
@@ -37,6 +37,7 @@ class User(MappedAsDataclass, Base):
 class UserInfo(BaseModel):
     full_name: str
 
+
 class Company(MappedAsDataclass, Base):
     __tablename__ = "Company"
     StockID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
@@ -47,6 +48,18 @@ class CompanyInfo(BaseModel):
     name: str
     ticker: str
 
+
+class Order(MappedAsDataclass, Base):
+    __tablename__ = "Order"
+    OrderID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, init=False)
+    UserID: Mapped[int] = mapped_column(Integer, nullable=False)
+    StockID: Mapped[int] = mapped_column(Integer, nullable=False)
+    IsBid: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+class OrderInfo(BaseModel):
+    user_id: int
+    stock_id: int
+    is_bid: bool
 
     
 @app.get('/get_users')
@@ -83,3 +96,20 @@ def add_user(payload: CompanyInfo) -> int:
         session.add(company)
         session.commit()
         return company.StockID
+    
+@app.get('/get_orders')
+def get_orders() -> list[dict, any]:
+    engine = create_engine(f'{base_url}/{db}')
+    with Session(engine) as session:
+        orders = session.scalars(select(Order)).all()
+        return [{'user_id': order.UserID, 'stock_id': order.StockID, 'order_id': order.OrderID, 'is_bid': order.IsBid} for order in orders]
+
+@app.post('/add_order')
+def add_order(payload: OrderInfo) -> int:
+    engine = create_engine(f'{base_url}/{db}')
+    print(f'Received Order Info: {payload}')
+    with Session(engine) as session:
+        order = Order(UserID=payload.user_id, StockID=payload.stock_id, IsBid=payload.is_bid)
+        session.add(order)
+        session.commit()
+        return order.StockID
