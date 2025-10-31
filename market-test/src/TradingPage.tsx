3d1d7fe48f9870278df346b89203c7efcdbe3c36
atsoc1993 <atsoc1993@gmail.com>
2025-10-31 11:15:17 -0400

    Removed unused imports

--- market-test/src/TradingPage.tsx
+++ market-test/src/TradingPage.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 type CompanyInfoResponse = {
